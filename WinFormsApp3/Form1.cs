67171e53366a25eb7fa383cde32a9c1ad55d1f65
LogiNRus <115283705+LogiNRus@users.noreply.github.com>
2022-10-17 19:07:05 +0300

    Калькулятор
    Калькулятор

---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,98 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public string D;
+        public string N1;
+        public bool N2;
+        public Form1()
+        {
+            N2 = false;
+            InitializeComponent();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (N2)
+            {
+                N2 = false;
+                textBox1.Text = "0";
+            }
+            Button B = (Button)sender;
+            if (textBox1.Text == "0")
+                textBox1.Text = B.Text;
+            else
+                textBox1.Text = textBox1.Text + B.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            D = B.Text;
+            N1 = textBox1.Text;
+            N2 = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double dn1, dn2, res;
+            res = 0;
+            dn1 = Convert.ToDouble(N1);
+            dn2 = Convert.ToDouble(textBox1.Text);
+            if (D == "+")
+            {
+                res = dn1 + dn2;
+            }
+            if (D == "-")
+            {
+                res = dn1 - dn2;
+            }
+            if (D == "*")
+            {
+                res = dn1 * dn2;
+            }
+            if (D == "/")
+            {
+                res = dn1 / dn2;
+            }
+            if (D == "%")
+            {
+                res = dn1 * dn2 / 100;
+            }
+            D = "=";
+            N2 = true;
+            textBox1.Text = res.ToString();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            double dn, res;
+            dn = Convert.ToDouble(textBox1.Text);
+            res = Math.Pow(dn, 2);
+            textBox1.Text = res.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains(","))
+                textBox1.Text = textBox1.Text + ",";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            if (textBox1.Text == "")
+            textBox1.Text = "0";
+        }
+    }
+}
